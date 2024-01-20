f29bd4f6e66359b4f278139c459245565d05aff6
JHF-INVEST <jhfinvest2023@gmail.com>
2024-01-19 23:12:54 -0300

    Inclusão da classes Base Entity Para ser abstrada e reutilizada para herença.

---
+++ ServiceSearchActions.Core/Entites/Shares/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceSearchActions.Core.Entites.Shares
+{
+    //Classe abstrata você não consegue instanciala apenas para conter informações que serão reutilizadas.
+    public abstract class BaseEntity
+    {
+        public int Id { get; private set; }
+        public BaseEntity()
+        {
+
+        }
+    }
+}
---
+++ ServiceSearchActions.Core/Entites/Shares/SummaryProfile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ServiceSearchActions.Core.Entites.Shares
+{
+    public class SummaryProfile : BaseEntity
+    {
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Sate { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Industry { get; set; }
+        public string IndustryKey { get; set; }
+        public string IndustryDisp { get; set; }
+        public string Sector { get; set; }
+        public string SectorKey { get; set; }
+        public string SectorDisp { get; set; }
+        public string LongBusinessSummary { get; set; }
+        public int FullTimeEmployees { get; set; }
+        public List<object> CompanyOfficers { get; set; }
+    }
+}
